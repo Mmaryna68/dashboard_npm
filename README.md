b70c2248c67c9f835fd217554b052130b4113c59
Mmaryna68 <maryov688@gmail.com>
2023-08-30 17:06:48 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # dashboard_npm
+
+# Project "Task Management Dashboard"
+
+![Example Project Screenshot](dashboard.png)
+![Example Project Screenshot](dashboard_task.png)
+
+This project is an interactive dashboard for managing tasks, built using HTML, CSS, and JavaScript. It visualizes task data through charts and provides a user-friendly interface for viewing and managing tasks.
+
+## Project Goals
+
+- Create a visually appealing and functional dashboard for task management.
+- Apply Chart.js, Moment.js, and Lodash libraries to create charts and handle data.
+- Provide a user-friendly interface for users.
+
+## Project Tasks
+
+- Build the HTML structure for the sidebar, charts, and task list.
+- Apply CSS for styling the interface and creating an appealing design.
+- Use JavaScript to create charts using the Chart.js library.
+- Process task data using Lodash.
+- Display task information, including date and description.
+- Output user profile data in the `footer` panel.
+
+## Installation and Running Instructions
+
+1. Clone the repository using the following command:
+   git clone https://github.com/Mmaryna68/dashboard_npm
+
+2. Navigate to the project directory:
+   cd dashboard_npm
+
+3. Install dependencies:
+   npm install
+
+4. Start the project:
+   npm start
+
+5. Open a web browser and go to `http://localhost:3000` to view the dashboard.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for details.
--- src/index.js
+++ src/index.js
@@ -5,42 +5,42 @@ import moment from "moment";
 import _ from "lodash";
 
 (async function () {
-  // Массив данных о задачах
+  // Array of task data
   const tasks = [
     {
-      name: "Task 1: Разработка интерфейса",
+      name: "Task 1: Interface development",
       progress: 70,
       date: "2023-06-10",
       priority: 2,
       status: "In Progress",
       description:
-        "Задача по разработке пользовательского интерфейса для нового веб-приложения. Необходимо создать прототипы, дизайн и реализовать интерфейс с использованием HTML, CSS и JavaScript.",
+        "The task of developing a user interface for a new web application. You need to prototype, design and implement the interface using HTML, CSS and JavaScript.",
     },
     {
-      name: "Task 2: Тестирование функционала",
+      name: "Task 2: Functionality testing",
       progress: 40,
       date: "2023-06-11",
       priority: 1,
       status: "Pending",
       description:
-        "Задача по тестированию функционала веб-приложения. Требуется написать и выполнить тестовые сценарии, выявить и исправить возможные ошибки и несоответствия требованиям.",
+        "The task of testing the functionality of a web application. It is required to write and execute test scenarios, identify and correct possible errors and inconsistencies with the requirements.",
     },
     {
-      name: "Task 3: Оптимизация производительности",
+      name: "Task 3: Performance optimization",
       progress: 90,
       date: "2023-06-12",
       priority: 3,
       status: "Completed",
       description:
-        "Задача по оптимизации производительности веб-приложения. Необходимо исследовать и улучшить скорость работы приложения, оптимизировать запросы к базе данных и устранить узкие места в коде.",
+        "The task of optimizing the performance of a web application. It is necessary to investigate and improve the speed of the application, optimize database queries and eliminate bottlenecks in the code.",
     },
   ];
 
-  // Создание графика с использованием Chart.js
+  // Creating a chart using Chart.js
   const ctx = document.getElementById("chart").getContext("2d");
 
-  // Обработка данных о задачах
-  const taskData = processData(tasks); // Обработка данных с помощью Lodash
+  // Processing task data
+  const taskData = processData(tasks); // Processing data with the Lodash
   const taskLabels = taskData.map((task) => task.label);
   const taskProgress = taskData.map((task) => task.progress);
 
@@ -50,7 +50,7 @@ import _ from "lodash";
       labels: taskLabels,
       datasets: [
         {
-          label: "Прогресс задач",
+          label: "Task progress",
           data: taskProgress,
           backgroundColor: "rgba(75, 192, 192, 0.6)",
         },
@@ -60,16 +60,16 @@ import _ from "lodash";
       scales: {
         y: {
           beginAtZero: true,
-          max: 100, // Максимальное значение шкалы установлено на 100
+          max: 100, // Maximum scale value set to 100
           ticks: {
-            stepSize: 10, // Шаг шкалы установлен на 10
+            stepSize: 10, // Scale step set to 10
           },
         },
       },
     },
   });
 
-  // Функция для обработки данных о задачах
+  // Function for progressing task data
   function processData(tasks) {
     const groupedTasks = _.groupBy(tasks, "status");
 
@@ -77,20 +77,20 @@ import _ from "lodash";
       return {
         label: status,
         count: tasks.length,
-        progress: calculateAverageProgress(tasks), // Добавление значения прогресса
+        progress: calculateAverageProgress(tasks), // Adding a progress value
       };
     });
 
     return processedData;
   }
 
-  // Функция для вычисления среднего значения прогресса
+  // Function for calclating the average progress value
   function calculateAverageProgress(tasks) {
     const totalProgress = tasks.reduce((sum, task) => sum + task.progress, 0);
     return Math.round(totalProgress / tasks.length);
   }
 
-  // Функция для отображения задач на странице
+  // Function for displaying tasks on a page
   function displayTasks() {
     const tasksContainer = document.getElementById("tasks");
 
@@ -100,7 +100,7 @@ import _ from "lodash";
     });
   }
 
-  // Функция для создания элемента задачи
+  // Function to create a task item
   function createTaskElement(task) {
     const taskElement = document.createElement("div");
     taskElement.classList.add("task");
@@ -111,16 +111,16 @@ import _ from "lodash";
 
     const statusElement = document.createElement("p");
     statusElement.classList.add("task-status");
-    statusElement.textContent = `Статус: ${task.status}`;
+    statusElement.textContent = `Status: ${task.status}`;
     taskElement.appendChild(statusElement);
 
     const dateElement = document.createElement("p");
-    const formattedDate = moment(task.date).format("DD.MM.YYYY"); // форматирование даты задачи с помощью moment
-    dateElement.textContent = `Дата: ${formattedDate}`;
+    const formattedDate = moment(task.date).format("DD.MM.YYYY"); // Formatting a task date using the Moment
+    dateElement.textContent = `Date: ${formattedDate}`;
     taskElement.appendChild(dateElement);
 
     const descriptionElement = document.createElement("p");
-    descriptionElement.textContent = `Описание: ${task.description}`;
+    descriptionElement.textContent = `Description: ${task.description}`;
     taskElement.appendChild(descriptionElement);
 
     const profileNameElement = document.getElementById("profile-name");
@@ -130,16 +130,16 @@ import _ from "lodash";
 
     return taskElement;
   }
-  // Вызов функции для отображения задач
+  // Calling a function to display tasks
   displayTasks();
 
-  // Функция для отображения данных о профиле
+  // Function to display profile data
   function displayProfile() {
     const profileNameElement = document.getElementById("profile-name");
     const profilePositionElement = document.getElementById("profile-position");
     profileNameElement.textContent = "Maryna O";
     profilePositionElement.textContent = "Web Developer";
   }
-  // Вызов функции для отображения данных о профиле
+  // Calling a function to display profile data
   displayProfile();
 })();
